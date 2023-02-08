8e6c682504ea956654d6acc83bf00c4635e61705
rus1an25 <mymessage02@gmail.com>
2023-02-08 11:07:47 +0200

    make it better

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+
+const webAppURL = `${process.env.WEB_APP_URL}`;
+const token = `${process.env.TOKEN}`;
+const bot = new TelegramBot(token, {polling: true});
+
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    bot.sendMessage(chatId,
+        `Hi, ${!msg.from.username ? msg.from.first_name : msg.from.username}✋\nI'll help you find the movies you need 😉`, {});
+
+    bot.sendMessage(chatId,
+        `👇Launch me👇`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: "Let's GO!", web_app: {url: webAppURL}}]
+                ]
+            }
+        });
+});
