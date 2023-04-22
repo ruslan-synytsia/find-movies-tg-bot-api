4899111a50e92f760371932bf22b81a9190c71c1
ruslan-synytsia <64065765+ruslan-synytsia@users.noreply.github.com>
2023-04-22 19:09:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
 const TelegramBot = require('node-telegram-bot-api');
 
-const webAppURL = `${process.env.WEB_APP_URL}`;
-const token = `${process.env.TOKEN}`;
-const bot = new TelegramBot(token, {polling: true});
+const app = express();
 
-bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    await bot.sendMessage(chatId,
-        `Hi, ${!msg.from.username ? msg.from.first_name : msg.from.username}✋\nI'll help you find the movies you need 😉`, {});
+app.use(bodyParser.json());
 
-    await bot.sendMessage(chatId,
-        `👇Launch me👇`,
-        {
-            reply_markup: {
-                inline_keyboard: [
-                    [{text: "Let's GO!", web_app: {url: webAppURL}}]
-                ]
-            }
-        });
+app.post('/webhook', (req, res) => {
+  const bot = new TelegramBot(process.env.TOKEN);
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
 });
 
-bot.on('polling_error', (error) => {
-  console.log(error);  // Вывод ошибки в консоль
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on port ${process.env.PORT}`);
+  
+  const bot = new TelegramBot(process.env.TOKEN);
+  bot.setWebHook(`${process.env.SERVER_URL}/webhook`);
+  console.log(`Webhook set to ${process.env.SERVER_URL}/webhook`);
 });
