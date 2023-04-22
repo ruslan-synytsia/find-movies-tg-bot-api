e80eec1713d7dd40141ded6814aab282950f5e4d
ruslan-synytsia <64065765+ruslan-synytsia@users.noreply.github.com>
2023-04-22 18:21:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
 require('dotenv').config();
-const express = require('express');
 const TelegramBot = require('node-telegram-bot-api');
-const http = require('http');
-const app = express();
-const server = http.createServer(app);
-
-const port = process.env.PORT || 5000;
 
 const webAppURL = `${process.env.WEB_APP_URL}`;
 const token = `${process.env.TOKEN}`;
 const bot = new TelegramBot(token, {polling: true});
 
-server.listen(port, () => {
-    bot.on('message', async (msg) => {
-        const chatId = msg.chat.id;
-        await bot.sendMessage(chatId,
-            `Hi, ${!msg.from.username ? msg.from.first_name : msg.from.username}✋\nI'll help you find the movies you need 😉`, {});
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    await bot.sendMessage(chatId,
+        `Hi, ${!msg.from.username ? msg.from.first_name : msg.from.username}✋\nI'll help you find the movies you need 😉`, {});
 
-        await bot.sendMessage(chatId,
-            `👇Launch me👇`,
-            {
-                reply_markup: {
-                    inline_keyboard: [
-                        [{text: "Let's GO!", web_app: {url: webAppURL}}]
-                    ]
-                }
-            });
-    });
+    await bot.sendMessage(chatId,
+        `👇Launch me👇`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: "Let's GO!", web_app: {url: webAppURL}}]
+                ]
+            }
+        });
 });
