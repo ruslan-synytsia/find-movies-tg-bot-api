5372856f87201502d77a75ad6d683be03d9d69c8
ruslan-synytsia <64065765+ruslan-synytsia@users.noreply.github.com>
2023-04-22 20:17:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const bot = new TelegramBot(process.env.TOKEN, {polling: true});
-
-bot.on('polling_error', (error) => {
-  console.log(error);
-});
+const bot = new TelegramBot(process.env.TOKEN);
 
 bot.setWebHook(`${process.env.SERVER_URL}/webhook`);
 console.log(`Webhook set to ${process.env.SERVER_URL}/webhook`);
