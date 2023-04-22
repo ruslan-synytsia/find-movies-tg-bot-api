8a0d99147b9c1c0a1494d82c54808be0049b6a9b
ruslan-synytsia <64065765+ruslan-synytsia@users.noreply.github.com>
2023-04-23 00:09:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const bot = new TelegramBot(token, { polling: true });
 
 // приветственное сообщение при старте бота
 bot.on('message', (msg) => {
-  console.log(token)
   const chatId = msg.chat.id;
   bot.sendMessage(chatId, `Привет, ${msg.from.first_name}! Я простой бот, который может отправлять сообщения.`);
+  if (msg.text === '/start') {
+    bot.sendMessage(chatId, "Стартуем!");
+  } else {
+    bot.sendMessage(chatId, `${msg.text}`);
+  }
 });
 
 // команда для отправки сообщения
