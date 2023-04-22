59c1d5ee59ad1498d62b56c557fd182063a4d312
ruslan-synytsia <64065765+ruslan-synytsia@users.noreply.github.com>
2023-04-22 20:28:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,9 +18,20 @@ app.post('/webhook', (req, res) => {
   res.sendStatus(200);
 });
 
-bot.on('message', (msg) => {
-  console.log(msg);
-  bot.sendMessage(msg.chat.id, 'Received your message!');
+bot.on('message', async (msg) => {
+    const chatId = msg.chat.id;
+    await bot.sendMessage(chatId,
+        `Hi, ${!msg.from.username ? msg.from.first_name : msg.from.username}✋\nI'll help you find the movies you need 😉`, {});
+
+    await bot.sendMessage(chatId,
+        `👇Launch me👇`,
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: "Let's GO!", web_app: {url: process.env.WEB_APP_URL}}]
+                ]
+            }
+        });
 });
 
 app.listen(process.env.PORT, () => {
